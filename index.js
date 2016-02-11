463d03e8bf400b034421158eaebf58f91fdec49d
JT Yim <JT@JTYim.com>
2016-02-10 21:11:00 -0500

    Deploy contd

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var app = require('./server.js');
 
- var port = 4568;
+// var port = 4568;
+// app.set("port", (process.env.PORT || 4568) );
+var port = process.env.PORT || 4568;
 
 app.listen(port);
 
