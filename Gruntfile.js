75e295c023c970d3ca0e4eb0534d10f7e1e2eb72
JT Yim <JT@JTYim.com>
2016-02-11 20:11:00 -0500

    deploy contd

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
         separator: ';'
       },
       dist: {
-        src: ['public/client/*.js'],
+        src: ['public/**/*.js'],
         // dest: '<% =distFolder %>.js'
         dest: 'public/dist/concat.js'
       },
@@ -95,6 +95,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-nodemon');
+
+  grunt.resgisterTask('heroku:production', [
+    'build'
+    ]);
 
   grunt.registerTask('server-dev', function (target) {
     // Running nodejs in a different process and displaying output on the main console
